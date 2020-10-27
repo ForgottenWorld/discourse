56a66f25710ba20dda7896450e9eae179474daa1
Enduvar <70820452+Enduvar@users.noreply.github.com>
2020-10-27 18:51:54 +0100

    Remove hardcoded limit from slug building function

--- app/assets/javascripts/discourse/app/models/category.js
+++ app/assets/javascripts/discourse/app/models/category.js
@@ -337,7 +337,7 @@ Category.reopenClass({
     return _uncategorized;
   },
 
-  slugFor(category, separator = "/", depth = 3) {
+  slugFor(category, separator = "/", depth) {
     if (!category) {
       return "";
     }
@@ -345,9 +345,12 @@ Category.reopenClass({
     const parentCategory = get(category, "parentCategory");
     let result = "";
 
-    if (parentCategory && depth > 1) {
-      result =
-        Category.slugFor(parentCategory, separator, depth - 1) + separator;
+    if (parentCategory) {
+      if (typeof depth === 'undefined') {
+        result = Category.slugFor(parentCategory, separator) + separator;
+      } else if (depth > 1) {
+        result = Category.slugFor(parentCategory, separator, depth - 1) + separator;
+      }
     }
 
     const id = get(category, "id"),
